1b65068b1188a9784da3b7b8fce1035f725c70d5
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-03-15 11:59:23 +0700

    feat encrypts user passwords

--- src/models/user/user.module.ts
+++ src/models/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, ConfigService],
 })
 export class UserModule {}
--- src/models/user/user.service.ts
+++ src/models/user/user.service.ts
@@ -3,17 +3,26 @@ import { UserDto } from './dto/user.dto';
 import { UserEntity } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
+import * as bycrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private userEntity: Repository<UserEntity>,
+    private configService: ConfigService,
   ) {}
   async create(dto: UserDto) {
     try {
+      // Mã hoá mật khẩu người dùng
+      console.log(typeof this.configService.get('SALT'));
+      const newPassword = await bycrypt.hash(
+        dto.password,
+        Number(this.configService.get('SALT')),
+      );
+
       // tạo người dùng mới
-      const newUser = this.userEntity.create(dto);
+      const newUser = this.userEntity.create({ ...dto, password: newPassword });
       await this.userEntity.save(newUser);
       return 'Tạo người dùng thành công';
     } catch (error) {
