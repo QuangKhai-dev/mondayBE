b7171f902caaffd7d965b8f47e32dfdafd4baed8
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-03-19 15:30:10 +0700

    feat: tao phuong thuc tim kiem bang email, chinh sua phuong thuc dang ky

--- src/models/user/user.service.ts
+++ src/models/user/user.service.ts
@@ -1,4 +1,8 @@
-import { BadGatewayException, Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  BadRequestException,
+  Injectable,
+} from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { UserEntity } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -13,30 +17,32 @@ export class UserService {
     private configService: ConfigService,
   ) {}
   async create(dto: UserDto) {
-    try {
-      // Kiểm tra email người dùng đã có trong hệ thống hay chưa
-      const checkExist = this.userEntity.findOne({
-        where: {
-          email: dto.email,
-        },
-      });
-      if (checkExist) {
-        throw new BadGatewayException('Email đã có trong hệ thống');
-      }
-
-      // Mã hoá mật khẩu người dùng
-      console.log(typeof this.configService.get('SALT'));
-      const newPassword = await bycrypt.hash(
-        dto.password,
-        Number(this.configService.get('SALT')),
-      );
-
-      // tạo người dùng mới
-      const newUser = this.userEntity.create({ ...dto, password: newPassword });
-      await this.userEntity.save(newUser);
-      return 'Tạo người dùng thành công';
-    } catch (error) {
-      throw new Error(error);
+    // Kiểm tra email người dùng đã có trong hệ thống hay chưa
+    const checkExist = await this.findUserByEmail(dto.email);
+    if (checkExist) {
+      throw new BadRequestException('Email đã có trong hệ thống');
     }
+
+    // Mã hoá mật khẩu người dùng
+    // console.log(typeof this.configService.get('SALT'));
+    const newPassword = await bycrypt.hash(
+      dto.password,
+      Number(this.configService.get('SALT')),
+    );
+
+    // tạo người dùng mới
+    const newUser = this.userEntity.create({ ...dto, password: newPassword });
+    await this.userEntity.save(newUser);
+    // remove password
+    delete newUser.password;
+    return newUser;
+  }
+
+  async findUserByEmail(email: string) {
+    return this.userEntity.findOne({
+      where: {
+        email,
+      },
+    });
   }
 }
